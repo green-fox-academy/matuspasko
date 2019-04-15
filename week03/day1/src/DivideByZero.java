18c44af4945998c07ad7e725920a2c5dc973d6fc
Matus Pasko <matuspasko@centrum.sk>
2019-04-15 13:55:11 +0200

    Tasl Divide By Zero

--- week03/day1/src/DivideByZero.java
+++ week03/day1/src/DivideByZero.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
+import java.util.*;
+
+// Create a function that takes a number
+// divides ten with it,
+// and prints the result.
+// It should print "fail" if the parameter is 0
 
 public class DivideByZero {
+    public static void main(String[] args) {
+        double divider = 10;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter number");
+        String number = scanner.nextLine();
+        double numberInput = Double.parseDouble(number);
+        double toCompare = (numberInput / divider);
+        if (toCompare == 0)
+            System.out.println("fail");
+        else
+            System.out.println("Resault is: " + toCompare);
+    }
 }
+
