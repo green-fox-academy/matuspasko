75c75a90c829d0b8b7b83efd813c999388e556d9
Matus Pasko <matuspasko@centrum.sk>
2019-04-26 13:03:38 +0200

    to do app

---
+++ week04/day03/src/main/java/Sum.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Sum {
+    List<Integer>inputNumbers;
+    public void Sum(List<Integer> inputNumbers){
+        this.inputNumbers = inputNumbers;
+    }
+
+
+}
---
+++ week04/day03/src/test/java/SumTest.java
@@ -0,0 +1,46 @@
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class SumTest {
+
+    @Test
+    public void Sum_test_list_of_arrays(){
+        List<Integer>integerList = new ArrayList<>();
+
+        List<Integer> result = integerList();
+
+
+
+        assertEquals(integerList, result);
+    }
+
+    @Test
+    public void Sum_test_empty_elements(){
+        List<Integer>emptyList = new ArrayList<>();
+
+
+    }
+
+    @Test
+    public void Sum_test_one_element(){
+        List<Integer>oneElementList = new ArrayList<>();
+    }
+
+    @Test
+    public void Sum_test_multiple_elements (){
+        List<Integer>multipleElements = new ArrayList<>();
+
+    }
+
+    @Test
+    public void Sum_test_null (){
+        List<Integer>nullInside = new ArrayList<>();
+
+    }
+
+
+}
