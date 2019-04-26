4c9d58d1913f12f917444023b62e29c9d3dfc446
Matus Pasko <matuspasko@centrum.sk>
2019-04-26 15:49:35 +0200

    to do app with -l argument and array training task

---
+++ week04/day04/src/Doubling.java
@@ -0,0 +1,20 @@
+
+public class Doubling {
+
+    public static void main(String[] args) {
+// - Create an integer variable named `baseNum` and assign the value `123` to it
+        int baseNum = 123;
+        Doubling(baseNum);
+    }
+
+    // - Create a function called `doubling` that doubles it's input parameter and returns with an integer
+    public static void Doubling(int baseNum) {
+        int doubleNum = baseNum * 2;
+        System.out.println(doubleNum);
+
+    }
+}
+// - Print the result of `doubling(baseNum)`
+
+
+
--- week04/day04/src/ToDoApp.java
+++ week04/day04/src/ToDoApp.java
@@ -1,2 +1,45 @@
+
+
+import java.io.IOException;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class ToDoApp {
+
+    public static void main(String[] args) {
+        Path pathTasks = Paths.get("/Users/matuspasko/Desktop/matuspasko/week04/day04/tasks.txt");
+
+        if (args.length == 0) {
+            System.out.println("Command Line Todo apllication");
+            System.out.println("==============================");
+            System.out.println("");
+            System.out.println("Comand line arguments:");
+            System.out.println("-l List all the tasks");
+            System.out.println("-a Adds a new task");
+            System.out.println("-r Remove a task");
+            System.out.println("-c Completes a task");
+
+        }
+        if (args[0].equals("-l")) {
+            try {
+                List<String> lines = Files.readAllLines(pathTasks);
+
+                if (lines.isEmpty()) {
+                    System.out.println("Not todo list for today.");
+                } else {
+
+                    for (int i = 0; i < lines.size(); i++) {
+                        System.out.println((i + 1) + " - " + lines.get(i));
+                    }
+                }
+            } catch (IOException e) {
+                System.out.println("Error file");
+            }
+
+        }
+
+    }
 }
