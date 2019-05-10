f1aa1598db95e38a183f0328f589cbb0ae701be7
Matus Pasko <matuspasko@centrum.sk>
2019-05-10 12:05:42 +0200

    matrix multiply

---
+++ trainingTasks/Matrix/src/main/java/MultiplyMatrix.java
@@ -0,0 +1,40 @@
+import java.util.Arrays;
+
+public class MultiplyMatrix {
+    public static void main(String[] args) {
+        int[][] inputMatrix = {{1, 3, 6, 2},
+                               {7, 5, 6, 1},
+                               {3, 3, 1, 5},
+                               {9, 0, 5, 3}};
+        multiplyMatrix(inputMatrix);
+        printMatrix(inputMatrix);
+
+//        System.out.println(Arrays.toString(multiplyMatrix(inputMatrix))); // v tomto pripade zbytocne
+    }
+
+    public static int[][] multiplyMatrix(int[][] inputMatrix) {
+        for (int row = 0; row < inputMatrix.length; row++) {
+            for (int columm = 0; columm < inputMatrix.length; columm++) {
+                if (row % 2 == 0 && inputMatrix[row][columm] % 2 == 0) {
+                    inputMatrix[row][columm] *= 2;
+                } else if (row % 2 != 0 && inputMatrix[row][columm] % 2 != 0) {
+                    inputMatrix[row][columm] *= 2;
+                }
+
+            }
+
+        }
+        return inputMatrix;
+
+    }
+
+    public static int[][] printMatrix(int[][] inputMatrix) {
+        for (int row = 0; row < inputMatrix.length; row++) {
+//            for (int columm = 0; columm < inputMatrix.length; columm++) {   // nepotrebujeme dva for loopy iba jeden
+            System.out.println(Arrays.toString(inputMatrix[row]));  // vdaka toString to vytlacime v tavare ako predtym
+
+        }
+        return inputMatrix;
+    }
+}
+
