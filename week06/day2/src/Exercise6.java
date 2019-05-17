2e4f98b3296bc520fba214b8e630c3620ea22547
Matus Pasko <matuspasko@centrum.sk>
2019-05-17 16:50:07 +0200

    created Stream Expression find upper char

--- week06/day2/src/Exercise6.java
+++ week06/day2/src/Exercise6.java
@@ -1,7 +1,19 @@
+import java.util.stream.Collectors;
+
 public class Exercise6 {
     public static void main(String[] args) {
 
+        String example = "HujghJGkkggJHjhjgJH";
 
+        // String example priradime ako charakter
+        String upperCase = example.chars()
+                // .filter vyberie z Character isUpperCase
+                .filter(Character::isUpperCase)
+                // .mapToObj objektovu hodnotu Stream pozostavajucu z aplikovania danej funkcie
+                .mapToObj(jaj -> Character.toString((char) jaj))
+                // collector vzdy pozbiera a vrati spat
+                .collect(Collectors.joining());
+        System.out.println(upperCase);
     }
 }
 // Write a Stream Expression to find the uppercase characters in a string!
